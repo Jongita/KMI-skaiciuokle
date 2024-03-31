76eaaef7cacbc4f6027ca0187c8907e29d112e4c
Jongita <jurgitajonaityte@gmail.com>
2024-03-31 20:01:07 +0300

    NaN included

--- skaiciuokle.js
+++ skaiciuokle.js
@@ -6,8 +6,13 @@ const kmi = document.getElementById("kmi");
 const skaiciuoti = () => {
     const height = heightInput.valueAsNumber / 100;
     const weight = weightInput.valueAsNumber;
-    const sum = (weight / height ** 2).toFixed(2);
-    kmi.innerHTML = `${isNaN(sum) ? 'Įveskite duomenis' : sum}`;
+    if (isNaN(height) || isNaN(weight)) {
+        kmi.innerHTML = `Įveskite skaičius`;
+    } else {
+        const sum = (weight / height ** 2).toFixed(2)
+        kmi.innerHTML = sum;
+    }
+
 }
 
 btn.onclick = skaiciuoti;
